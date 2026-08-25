b62a4f6fd5ab6d3e4b513401c96b6aa85e5ffc27
Thales Carvalho <noahkaneki134@gmail.com>
2026-08-25 12:51:30 -0300

    primeiros passos - kotlin

---
+++ hora de codar/src/main/kotlin/1.1.kt
@@ -0,0 +1,9 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val nome_do_carro = "Fusca"
+    println(nome_do_carro)
+
+}
---
+++ hora de codar/src/main/kotlin/1.2.kt
@@ -0,0 +1,8 @@
+package org.example
+
+fun main(){
+    println("Qual é seu nome?")
+    val nome_do_usuario = readln()
+    println("Olá, $nome_do_usuario")
+}
+
